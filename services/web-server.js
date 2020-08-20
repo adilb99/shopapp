65d9b18bb4257d82390bcec7874eb396f6aba5aa
Adil Botabekov <botabekov.adil@gmail.com>
2020-08-20 11:29:54 +0000

    added CORS

--- services/web-server.js
+++ services/web-server.js
@@ -18,6 +18,12 @@ function initialize() {
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
 
+    app.use(function(req, res, next){
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    })
+
     app.use('/api', router);
     app.get('/', async (req, res) => {
       const result = await database.simpleExecute('select name from product');
